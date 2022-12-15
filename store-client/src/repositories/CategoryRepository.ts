1dc93fabc467f5ff6dc12ad95b9536f2e3a6bc14
ทนงศักดิ์ แสงศักดิ์ <aomsinsaengsak@gmail.com>
2022-12-15 21:13:26 +0700

    ทำให้ใช้ข้อมูลจาก Backend ได้

--- store-client/src/repositories/CategoryRepository.ts
+++ store-client/src/repositories/CategoryRepository.ts
@@ -1,33 +1,38 @@
 import Category from "../models/Category";
 import { IRepository } from "./IRepository";
-
+import config from "../config";
+import axios from 'axios';
 
 export class CategoryRepository implements IRepository<Category> {
-    async getAll(): Promise<Category[] | null> {
-        
-        return [
-            {id: 1, title: 'Fantasy'},
-            {id: 2, title: 'Computer'}
-        ]
-    }
-    async get(id: number|string): Promise<Category | null>{
-        const result = {id: 1, title: 'Fantasy'}
+  urlPrefix = config.remoteRepositoryUrlPrefix
 
-        return result
-    }
-    async create(entity: Partial<Category>): Promise<void> {
-        const result = {id: 3, title: 'Action'}
+  async getAll(): Promise<Category[] | null>{
+    const result = await axios.get<Category[]>(`${this.urlPrefix}/category`)
 
-        console.log(result)
-    }
-    async update(entity: Partial<Category>): Promise<void>{
-        const result = {id: 1, title: 'Romantic'}
+    return result.data
+  }
 
-        console.log(result)
-    }
-    async delete(id: number|string): Promise<void>{
-        const result = {id: 1, title: 'Fantasy'}
+  async get(id: number|string): Promise<Category | null>{
+    const result = await axios.get<Category>(`${this.urlPrefix}/category/${id}`)
+    
+    return result.data
+  }
 
-        console.log(result)
-    }
+  async create(entity: Partial<Category>): Promise<void>{
+    const result = await axios.post<Category>(`${this.urlPrefix}/category`, entity)
+    console.log(result.data)
+  }
+
+  async update(entity: Partial<Category>): Promise<void>{
+    const result = await axios.put<Category>(
+      `${this.urlPrefix}/category/${entity.id}`, 
+      entity
+    )
+    console.log(result.data)
+  }
+
+  async delete(id: number|string): Promise<void>{
+    const result = await axios.delete<Category>(`${this.urlPrefix}/category/${id}`)
+    console.log(result.data)
+  }
 }
