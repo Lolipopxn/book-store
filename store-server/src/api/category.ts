6a92d66eb13a2894677c19d11ba8552ca7a2f7b6
ทนงศักดิ์ แสงศักดิ์ <aomsinsaengsak@gmail.com>
2022-12-15 17:37:48 +0700

    เพิ่ม .post .put .del ใน category api

--- store-server/src/api/category.ts
+++ store-server/src/api/category.ts
@@ -15,6 +15,30 @@ router
     }
     ctx.body = category
   })
+  .post('/', async (ctx, next) => {    
+    const result = await db('category').insert(ctx.request.body)
+    if(result.length <= 0){
+      ctx.response.status = 400
+      return
+    }
+    const id = result[0]
+    ctx.body = await db('category').select('*').where(id).first()
+  })
+  .put('/:id', async (ctx, next) => {
+    const id = parseInt(ctx.params.id)
+    delete ctx.request.body.id
+    const rowUpdated = await db('category').select('*').where(id).update(ctx.request.body)
+    if(rowUpdated == 0){
+      ctx.response.status = 404
+      return
+    }
+    ctx.body = await db('category').select('*').where(id).first()
+  })
+  .del('/:id', async (ctx, next) => {
+    const id = parseInt(ctx.params.id)
+    const rowUpdated = await db('category').select('*').where(id).del()
+    ctx.body = {statusCode: rowUpdated > 0 ? 1 : 0}
+  })
 
 
 export default router
