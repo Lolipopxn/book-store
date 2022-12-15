2d4fe0278403ea814976f0c359a0ef530a109b2b
ทนงศักดิ์ แสงศักดิ์ <aomsinsaengsak@gmail.com>
2022-12-15 17:40:38 +0700

    ทำให้ category api สามารถ makeQuery ได้

--- store-server/src/api/category.ts
+++ store-server/src/api/category.ts
@@ -2,13 +2,16 @@ import Router from 'koa-router'
 import db from '../db'
 const router = new Router()
 
+const makeQuery = () => db('category').select('*')
+const findById = (id: number) => makeQuery().where({id})
+
 router
   .get('/', async (ctx, next) => {            
-    ctx.body = await db('category').select('*').orderBy('id')
+    ctx.body = await makeQuery().orderBy('id')
   })
   .get('/:id', async (ctx, next) => {
     const id = parseInt(ctx.params.id)
-    const category = await db('category').select('*').where(id).first()
+    const category = await findById(id).first()
     if(!category){
       ctx.response.status = 404
       return
@@ -22,21 +25,21 @@ router
       return
     }
     const id = result[0]
-    ctx.body = await db('category').select('*').where(id).first()
+    ctx.body = await findById(id).first()
   })
   .put('/:id', async (ctx, next) => {
     const id = parseInt(ctx.params.id)
     delete ctx.request.body.id
-    const rowUpdated = await db('category').select('*').where(id).update(ctx.request.body)
+    const rowUpdated = await findById(id).update(ctx.request.body)
     if(rowUpdated == 0){
       ctx.response.status = 404
       return
     }
-    ctx.body = await db('category').select('*').where(id).first()
+    ctx.body = await findById(id).first()
   })
   .del('/:id', async (ctx, next) => {
     const id = parseInt(ctx.params.id)
-    const rowUpdated = await db('category').select('*').where(id).del()
+    const rowUpdated = await findById(id).del()
     ctx.body = {statusCode: rowUpdated > 0 ? 1 : 0}
   })
 
