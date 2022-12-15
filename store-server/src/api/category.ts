61a3f11d86d407207430667ba8d793fc7064eab3
ทนงศักดิ์ แสงศักดิ์ <aomsinsaengsak@gmail.com>
2022-12-15 17:28:20 +0700

    แก้ category api ให้เรียกใช้ db ได้

--- store-server/src/api/category.ts
+++ store-server/src/api/category.ts
@@ -1,12 +1,20 @@
 import Router from 'koa-router'
+import db from '../db'
 const router = new Router()
 
 router
-    .get('/', async (ctx, next) =>{
-        ctx.body = [
-            {id: 1, title: 'Programming'},
-            {id: 2, title: 'Life Style'}
-        ]
-    })
+  .get('/', async (ctx, next) => {            
+    ctx.body = await db('category').select('*').orderBy('id')
+  })
+  .get('/:id', async (ctx, next) => {
+    const id = parseInt(ctx.params.id)
+    const category = await db('category').select('*').where(id).first()
+    if(!category){
+      ctx.response.status = 404
+      return
+    }
+    ctx.body = category
+  })
 
-    export default router
+
+export default router
