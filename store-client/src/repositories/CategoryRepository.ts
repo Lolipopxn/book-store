396e32fc711d25c1b517e5efc86b44778970c7e8
ทนงศักดิ์ แสงศักดิ์ <aomsinsaengsak@gmail.com>
2022-12-15 20:55:45 +0700

    เพิ่ม method ใน category repo

--- store-client/src/repositories/CategoryRepository.ts
+++ store-client/src/repositories/CategoryRepository.ts
@@ -5,9 +5,29 @@ import { IRepository } from "./IRepository";
 export class CategoryRepository implements IRepository<Category> {
     async getAll(): Promise<Category[] | null> {
         
-        return[
+        return [
             {id: 1, title: 'Fantasy'},
             {id: 2, title: 'Computer'}
         ]
+    }
+    async get(id: number|string): Promise<Category | null>{
+        const result = {id: 1, title: 'Fantasy'}
+
+        return result
+    }
+    async create(entity: Partial<Category>): Promise<void> {
+        const result = {id: 3, title: 'Action'}
+
+        console.log(result)
+    }
+    async update(entity: Partial<Category>): Promise<void>{
+        const result = {id: 1, title: 'Romantic'}
+
+        console.log(result)
+    }
+    async delete(id: number|string): Promise<void>{
+        const result = {id: 1, title: 'Fantasy'}
+
+        console.log(result)
     }
 }
