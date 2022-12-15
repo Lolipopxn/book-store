ed89a5dda5abea9e6f1df945de8287a3e75721c7
ทนงศักดิ์ แสงศักดิ์ <aomsinsaengsak@gmail.com>
2022-12-15 19:15:00 +0700

    แสดงข้อมูล Category ทั้งหมด

--- store-client/src/App.tsx
+++ store-client/src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import Repo from './repositories'
+import Category from './models/Category';
+
 
 function App() {
+  const [categoryList, setCategoryList] = useState<Category[]>([])
+
+  const fetchCategoryList = async () => {
+    const result = await Repo.categories.getAll()
+    if (result) {
+      setCategoryList(result)
+    }
+  }
+  useEffect(() => {
+    fetchCategoryList()
+  },)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+          {categoryList.map(category => <option key={category.id} value={category.id}>{category.title}</option>)}
     </div>
   );
 }
