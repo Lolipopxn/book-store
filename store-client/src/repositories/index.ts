63e850eb4330cdba90a0b1dbc97fbdefb814d63f
ทนงศักดิ์ แสงศักดิ์ <aomsinsaengsak@gmail.com>
2022-12-17 16:05:21 +0700

    STEP 2

--- store-client/src/repositories/index.ts
+++ store-client/src/repositories/index.ts
@@ -1,7 +1,6 @@
 import { CategoryRepository } from "./CategoryRepository"
 import { BookRepository } from "./BookRepository";
 
-
 const repositories = {
   categories: new CategoryRepository(),
   books: new BookRepository()
