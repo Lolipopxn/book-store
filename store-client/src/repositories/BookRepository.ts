b5ae9a98d01cced8869f9c8ca812682aa49d6921
ทนงศักดิ์ แสงศักดิ์ <aomsinsaengsak@gmail.com>
2022-12-17 15:26:10 +0700

    เติม Book Repo ให้มีครบทั้ง 5 method

--- store-client/src/repositories/BookRepository.ts
+++ store-client/src/repositories/BookRepository.ts
@@ -12,4 +12,37 @@ export class BookRepository implements IRepository<Book> {
 
         ]
     }
+    async get(id: number|string): Promise<Book | null>{
+        const result = {
+            id: 1, title: 'Harry Potter', price: 560, stockAmount: 10,
+            category: { id: 1, title: 'Fantasy'}
+        }
+        
+        return result
+      }
+    
+      async create(entity: Partial<Book>): Promise<void>{
+        const result = {
+            id: 1, title: 'Harry Potter', price: 560, stockAmount: 10,
+            category: { id: 1, title: 'Fantasy'}
+        } 
+        console.log(result)
+      }
+    
+      async update(entity: Partial<Book>): Promise<void>{
+        const result = {
+            id: 1, title: 'Harry Potter', price: 560, stockAmount: 10,
+            category: { id: 1, title: 'Fantasy'}
+        } 
+
+        console.log(result)
+      }
+    
+      async delete(id: number|string): Promise<void>{
+        const result = {
+            id: 1, title: 'Harry Potter', price: 560, stockAmount: 10,
+            category: { id: 1, title: 'Fantasy'}
+        }
+        console.log(result)
+      }
 }
