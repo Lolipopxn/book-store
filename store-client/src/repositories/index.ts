9e5166ba3a31ad93e69813cecc53512f24f739fb
ทนงศักดิ์ แสงศักดิ์ <aomsinsaengsak@gmail.com>
2022-12-17 15:24:04 +0700

    สร้าง BookDetail เเล้วเอาข้อมูลที่ mock มาเเสดง

--- store-client/src/repositories/index.ts
+++ store-client/src/repositories/index.ts
@@ -1,8 +1,10 @@
 import { CategoryRepository } from "./CategoryRepository"
+import { BookRepository } from "./BookRepository";
 
 
 const repositories = {
   categories: new CategoryRepository(),
+  books: new BookRepository()
 }
 
 export default repositories
